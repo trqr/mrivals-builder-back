9f89ef89e959b80190d97aa07a690ebffc0b3a66
TrqR <tristanqr@gmail.com>
2025-08-21 14:26:02 +0200

    first commit, initialisation du projet

---
+++ src/main/java/com/mrivals_builder/Mrivals_Builder/MrivalsBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.mrivals_builder.Mrivals_Builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrivalsBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrivalsBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrivals_builder/Mrivals_Builder/MrivalsBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrivals_builder.Mrivals_Builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrivalsBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
