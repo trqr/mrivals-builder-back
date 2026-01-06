a976eb932a7b8a61711626b8b92a07ca2bd9f695
TrqR <tristanqr@gmail.com>
2026-01-06 15:36:58 +0100

    putting 12 char min on pw

--- src/main/java/com/mrivals_builder/Mrivals_Builder/dtos/AuthDTOs/RegisterRequestDTO.java
+++ src/main/java/com/mrivals_builder/Mrivals_Builder/dtos/AuthDTOs/RegisterRequestDTO.java
@@ -17,8 +17,8 @@ public record RegisterRequestDTO(
 
         @NotBlank(message = "Password cannot be empty.")
         @Pattern(
-                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&._-])[A-Za-z\\d@$!%*?&._-]{8,}$",
-                message = "Password must be at least 8 characters long, containing at least one uppercase letter, one lowercase letter, one number, and one special character."
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&._-])[A-Za-z\\d@$!%*?&._-]{12,}$",
+                message = "Password must be at least 12 characters long, containing at least one uppercase letter, one lowercase letter, one number, and one special character."
         )
         String password
 ) {}
